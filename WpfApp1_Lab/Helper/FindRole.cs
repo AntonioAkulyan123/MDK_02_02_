e393b97707e33328af46384bf4d80cfb8f28047c
AntonioAkulyan123 <ninka.544@mail.ru>
2024-04-18 10:40:02 +0900

    Лаб 2 и 3

---
+++ WpfApp1_Lab/Helper/FindRole.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp1_Lab.Model;
+
+namespace WpfApp1_Lab.Helper
+{
+    public class FindRole
+    {
+        int id;
+        public FindRole(int id)
+        {
+            this.id = id;
+        }
+        public bool RolePredicate(Role role)
+        {
+            return role.Id == id;
+        }
+    }
+}
+
